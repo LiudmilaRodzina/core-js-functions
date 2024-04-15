fe90e4f051ee2bbf6d63b7d634514846dd18ccec
Liudmila Rodzina <liudmilarodzina@gmail.com>
2024-04-15 23:00:21 +0300

    feat: add solution to 6-7 tasks

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -182,8 +182,10 @@ function logger(/* func, logFunc */) {
  *   partialUsingArguments(fn, 'a','b','c')('d') => 'abcd'
  *   partialUsingArguments(fn, 'a','b','c','d')() => 'abcd'
  */
-function partialUsingArguments(/* fn, ...args1 */) {
-  throw new Error('Not implemented');
+function partialUsingArguments(fn, ...args1) {
+  return function applyPartial(...args2) {
+    return fn(...args1, ...args2);
+  };
 }
 
 /**
@@ -203,8 +205,14 @@ function partialUsingArguments(/* fn, ...args1 */) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let currentId = startFrom;
+
+  return function getNextId() {
+    const result = currentId;
+    currentId += 1;
+    return result;
+  };
 }
 
 module.exports = {
